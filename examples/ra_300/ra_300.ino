4671ccd755ed9622b330b8bc19142928e3510229
rob tillaart <rob.tillaart@gmail.com>
2021-05-19 11:48:30 +0200

    examples

---
+++ examples/ra_300/ra_300.ino
@@ -0,0 +1,39 @@
+//
+//    FILE: ra_getValue.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.0
+//    DATE: 2020-01-15
+//
+// PUPROSE: demonstrate access in order of the values added
+//
+
+#include "RunningAverage.h"
+
+RunningAverage myRA(300);
+int samples = 0;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Demo RunningAverage lib");
+  Serial.print("Version: ");
+  Serial.println(RUNNINGAVERAGE_LIB_VERSION);
+  
+  myRA.clear();
+  for (uint16_t i = 0; i < 1000; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+}
+
+void loop(void)
+{
+}
+
+// -- END OF FILE --
---
+++ examples/ra_partial/ra_partial.ino
@@ -0,0 +1,93 @@
+//
+//    FILE: ra_partial.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.0
+//    DATE: 2020-01-15
+//
+// PUPROSE: demonstrate access in order of the values added
+//
+
+#include "RunningAverage.h"
+
+RunningAverage myRA(25);
+int samples = 0;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Demo RunningAverage lib");
+  Serial.print("Version: ");
+  Serial.println(RUNNINGAVERAGE_LIB_VERSION);
+
+  myRA.clear();
+  for (uint16_t i = 0; i < 30; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+  Serial.println();
+
+  myRA.setPartial(20);
+  for (uint16_t i = 0; i < 30; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+  Serial.println();
+
+  myRA.setPartial(50);
+  for (uint16_t i = 0; i < 30; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+  Serial.println();
+
+  myRA.setPartial(10);
+  for (uint16_t i = 0; i < 30; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+  Serial.println();
+
+  myRA.setPartial();
+  for (uint16_t i = 0; i < 30; i++)
+  {
+    myRA.addValue(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(myRA.getCount());
+    Serial.print("\t");
+    Serial.print(myRA.getAverage());
+    Serial.println();
+  }
+  Serial.println();
+
+}
+
+void loop(void)
+{
+}
+
+// -- END OF FILE --
